1aa3e4e304f3eb94101e7525c5222cf11d308e6b
Anushkothuri <kothurianush16@gmail.com>
2024-05-07 18:10:05 +0530

    array

---
+++ array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int arr[4]={5,6,2,4};
+    printf("enter elements in array:");
+    int i;
+    for(i=0;i<4;i++)
+    {
+        printf("%d",arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
